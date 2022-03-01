41ff97a5406e7ad9bfa157dd24a8244beca7a9eb
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-03-01 10:21:17 +0100

    Updates

--- python-scripts/text-to-json-3.py
+++ python-scripts/text-to-json-3.py
@@ -23,7 +23,7 @@ IPV6ADDR = '|'.join(['(?:{})'.format(g) for g in IPV6GROUPS[::-1]])  # Reverse r
 flow_label_list = []
 # reg = r"((([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+([0-9a-fA-F]+))+"
 reg = r"(((([0-9a-f]) ?){32})\n){6}"
-pattern = re.compile(reg, re.MULTILINE)
+pattern = re.compile(reg)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
@@ -38,56 +38,49 @@ file = args.file
 # file parsing starts here
 with open(file, "r") as my_file:
     data = my_file.read()
-    match = re.search(pattern, data)
-    #print(match)
-    #print(match.group())
-    #print(re.split(pattern, data))
-
-    for item in re.finditer(pattern, data):
-        # print(item.group())
-        ip = (item.group()[24:72].replace(" ", "")).replace("\n", "")
-        ipv6_addr = ipaddress.ip_address(int(ip, 16))
-        fl = item.group()[151:158].replace(" ", "")
-        #print(ip)
-        #print(fl)
-        tuple = (str(ipv6_addr), fl)
-        flow_label_list.append(tuple)
     
     # create and populate list of returned flow-labels as a (ipv6-address, returned_flow_label) tuple
-    #flow_labels = ["".join(x) for x in re.findall(pattern, data)]
+    flow_labels = ["".join(x) for x in re.findall(pattern, data)]
+    for index, item in enumerate(flow_labels):
+        size = len(flow_labels[index])
+        #print(a[index][:size - 37])
+        flow_labels[index] = flow_labels[index][:size - 37]
+        #print(f"Index:\t{index}")
+        #print(a[index])
 
-    #print(flow_labels)
+        # get the destination address, flow label tuple from the output
+        #tuple = (a[index][151:158].replace(" ", ""), (a[index][24:72].replace(" ", "")).replace("\n", ""))
 
-    #for index, item in enumerate(flow_labels):
-        #size = len(flow_labels[index])
-        #flow_labels[index] = flow_labels[index][:size - 37]
+        ip = (flow_labels[index][24:72].replace(" ", "")).replace("\n", "")
 
-        #ip = (flow_labels[index][24:72].replace(" ", "")).replace("\n", "")
+        ipv6_addr = ipaddress.ip_address(int(ip, 16))
+        #print(ipv6_addr)
 
-        #ipv6_addr = ipaddress.ip_address(int(ip, 16))
-
-        #tuple = (str(ipv6_addr), flow_labels[index][151:158].replace(" ", ""))
+        tuple = (str(ipv6_addr), flow_labels[index][151:158].replace(" ", ""))
         #print("Tuple:")
         #print(tuple)
-        #flow_label_list.append(tuple)
+        flow_label_list.append(tuple)
     
-    ## remove duplicate items from flow_label_list
+    # remove duplicate items from flow_label_list
     flow_label_list = list(dict.fromkeys(flow_label_list))
-    #print(len(flow_label_list))
-    #print(flow_label_list)
 
-    ## create hop-list (list of ipv6_addresses in path)
+    #result = re.findall(IPV6ADDR, data)
+    #print(result)
+
+    # create hop-list (list of ipv6_addresses in path)
+    #hop_list = my_file.read().splitlines() 
     hop_list = re.findall(IPV6ADDR, data) 
-    ## remove duplicates
+    # remove duplicates
     hop_list = list( dict.fromkeys(hop_list) )
-    ## Remove first item in the list (the destination address) and add it as separate dictionary element
+    # Remove first item in the list (the destination address) and add it as separate dictionary element
     dest = hop_list.pop(0)
 
     #print("Hop list:")
     #print(hop_list)
 
-    ## Create top-level dictionary
+    # Create top-level dictionary
     my_dict = {}
+    #my_dict["probe_uuid"] = str(uuid.uuid4())
 
     my_dict["outgoing_tcp_port"] = args.tcp_port
     my_dict["flow_label"] = args.flow_label
@@ -95,27 +88,25 @@ with open(file, "r") as my_file:
     my_dict["source"] = args.source_ip
     my_dict["destination"] = dest
 
-    #count = 0 # in case items is empty and you need it after the loop
+    count = 0 # in case items is empty and you need it after the loop
+    #hop_dictionary = { index : item for index, item in enumerate(hop_list, start=1) }
+    #hop_dictionary = { index : {address : flow_label_list[index][1]} for index, address in enumerate(hop_list, start=1) if (address == flow_label_list[0][0])}
 
-    ## Initialize hop dictionary
+    # Initialize hop dictionary
     hop_dictionary = { index : {"ipv6_address" : address, "returned_flow_label" : "null"} for index, address in enumerate(hop_list, start=1)}
 
-    #index = 0
-    for index, address in enumerate(hop_list):
-        if flow_label_list[index][0] == address:
+    index = 0
+    for item in hop_list:
+        if hop_dictionary[index+1]["ipv6_address"] == item:
             hop_dictionary[index+1]["returned_flow_label"] = int(flow_label_list[index][1], 16)
-        #index = index + 1
-    #for item in hop_list:
-        #if hop_dictionary[index+1]["ipv6_address"] == item:
-            #hop_dictionary[index+1]["returned_flow_label"] = int(flow_label_list[index][1], 16)
-        #index = index + 1
+        index = index + 1
 
-    print("Hop dictionary:")
-    print(hop_dictionary)
+    #print("Hop dictionary:")
+    #print(hop_dictionary)
 
 my_dict["hops"] = hop_dictionary
-print("Complete dictionary:")
-print(my_dict)
+#print("Complete dictionary:")
+#print(my_dict)
 
 json_file = args.file
 if json_file.endswith('.txt'):
