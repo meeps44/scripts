735c800c5ca6bfd3a466225aca73db28b1c525cf
Erlend <erlend_hapnes@hotmail.com>
2022-03-06 22:34:10 +0100

    Updates

--- python-scripts/asn-to-advertised-prefixes.py
+++ python-scripts/asn-to-advertised-prefixes.py
@@ -3,5 +3,4 @@ import urllib.request
 # gets a list of ipv6-prefixes advertised by each asn (up to and including asn 35000)
 
 # the file the list gets saved to
-file = "/root/git/scripts/text-files/ip-asn-map.json"
-
+file = "/root/git/scripts/text-files/ASNMap.json"
--- python-scripts/ip-to-as.py
+++ python-scripts/ip-to-as.py
@@ -4,11 +4,8 @@ import urllib.request
 # the result, along with the AS advertising this specifix prefix is logged
 # to a key-value dictionary.
 
-# goes through a list of IP-addresses and gets which AS (Autonomous System)
-# the IP-address belongs to, up to a maximum of 35000 ASes. 
-# the script will get one IP-address per AS.
-
 # file = "/root/git/scripts/text-files/responsive-addresses-test.txt"
+# file = "/home/erlend/git/scripts/text-files/responsive-addresses-test.txt"
 file = "/home/erlend/git/scripts/text-files/responsive-addresses-test.txt"
 url = r"https://api.hackertarget.com/aslookup/?q="
 
@@ -18,12 +15,3 @@ with open(file, "r") as my_file:
 		with urllib.request.urlopen(f"{url}{line}") as response:
 			body = response.read()
 			print(body)
-
-#dict = {
-	#"asn":2549,
-	#"prefixes": [
-		#"2001:1210:105:34:0:606:a8:31"
-		#"2001:1210::1:1"
-	#],
-	#"AS range":"2001:1210::1:1"
-#}
