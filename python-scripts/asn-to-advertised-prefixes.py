d7ea33b4d85623af8f08755c1e855dbe54ea6860
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-03-07 11:54:45 +0100

    Some fixes and added asnmap.json

--- python-scripts/asn-to-advertised-prefixes.py
+++ python-scripts/asn-to-advertised-prefixes.py
@@ -1,6 +1,21 @@
 import urllib.request
 
-# gets a list of ipv6-prefixes advertised by each asn (up to and including asn 35000)
+# gets a list of ipv6-prefixes advertised by each public asn (up to and including asn 35000)
 
 # the file the list gets saved to
-file = "/root/git/scripts/text-files/ASNMap.json"
+# asnmap = "/root/git/scripts/text-files/asnmap.json"
+asnmap = "/Users/admin/git/scripts/text-files/asnmap.json"
+
+# file = "/root/git/scripts/text-files/responsive-addresses-test.txt"
+# file = "/home/erlend/git/scripts/text-files/responsive-addresses-test.txt"
+# file = "/home/erlend/git/scripts/text-files/responsive-addresses-test.txt"
+# file = "/root/git/scripts/text-files/full-as-list.txt"
+
+# ip_url = r"https://api.hackertarget.com/aslookup/?q="
+asn_url = r"https://api.hackertarget.com/aslookup/?q=AS"
+
+with open(asnmap, "a") as asnmap:
+    for asn in range(32934, 32936):
+        with urllib.request.urlopen(f"{asn_url}{asn}") as response:
+            body = response.read()
+            asnmap.write(f"{str(body)}\n")
