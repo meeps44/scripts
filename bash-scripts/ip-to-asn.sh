f3b8e0c963c55ba580af9a5d3f0c896302a7a2fe
Erlend <erlend_hapnes@hotmail.com>
2022-03-08 23:09:02 +0100

    Small update

--- bash-scripts/ip-to-asn.sh
+++ bash-scripts/ip-to-asn.sh
@@ -5,5 +5,14 @@ END=3
 # Iterates from 1 up to and including $END
 for i in $(seq 1 $END); do 
 	echo $i 
-	echo $(whois 240e:398:303:44ac:7e03:c9f1:238f:b530 | grep origin)
+done
+
+# Iterates through each line in a text file
+cat /home/erlend/python-programming/ipv6-addresses-test.txt | while read line; do
+	# echo $i 
+	echo "IP: $line"
+	echo $(whois $line | grep origin)
+	
+	# grep multiple patterns:
+	echo $(whois $line | grep -E 'origin|route6')
 done
