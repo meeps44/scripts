32dc5613d30d81a022937d48dafa0b4a4cf2719e
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-03-09 13:20:52 +0100

    Update

--- bash-scripts/ip-to-asn.sh
+++ bash-scripts/ip-to-asn.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+ASNMAP="/root/git/scripts/text-files/asnmap.txt"
+
 END=3
 
 # Iterates from 1 up to and including $END
@@ -7,7 +9,7 @@ for i in $(seq 1 $END); do
 	echo $i 
 done
 
-# Iterates through each line in a text file
+# Iterates through each line in a text file and gets the ASN using WHOIS
 cat /home/erlend/python-programming/ipv6-addresses-test.txt | while read line; do
 	# echo $i 
 	echo "IP: $line"
@@ -15,4 +17,11 @@ cat /home/erlend/python-programming/ipv6-addresses-test.txt | while read line; d
 	
 	# grep multiple patterns:
 	echo $(whois $line | grep -E 'origin|route6|Origin')
+done
+
+# Iterates through each line in a text file and gets the ASN using nitefood's ASN lookup tool
+# https://github.com/nitefood/asn
+cat /home/erlend/python-programming/ipv6-addresses-test.txt | while read line; do
+	# echo $i 
+	echo "IP: $line ASN: $(asn $line | grep ASN)" >> $ASNMAP
 done
