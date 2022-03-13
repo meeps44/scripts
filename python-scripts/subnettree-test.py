f3dcf4f85fc2e13f127e6ddfdc4b69f046a47864
Erlend <erlend_hapnes@hotmail.com>
2022-03-13 11:44:18 +0100

    Added subnettree script

---
+++ python-scripts/subnettree-test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from pathlib import Path
+
+import argparse
+import sys
+
+try:
+    import SubnetTree
+except Exception as e:
+    print(e, file=sys.stderr)
+    print("Use `pip install pysubnettree` to install the required module", file=sys.stderr)
+    sys.exit(1)
+
+
+def fill_tree(tree, fh):
+    for line in fh:
+        line = line.strip()
+        try:
+            tree[line] = line
+        except ValueError as e:
+            print("Skipped line '" + line + "'", file=sys.stderr)
+    return tree
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--prefix-file", required=True, type=argparse.FileType('r'), help="File containing list of RouteViews prefixes")
+    parser.add_argument("-i", "--ip-address-file", required=True, type=argparse.FileType('r'), help="File containing IP addresses to be matched against (non-)aliased prefixes")
+    args = parser.parse_args()
+
+    # Store aliased and non-aliased prefixes in a single subnet tree
+    tree = SubnetTree.SubnetTree()
+
+    # Read aliased and non-aliased prefixes
+    tree = fill_tree(tree, args.prefix_file)
+
+    # Set up outputfile
+    filename = f"{str(Path.home())}/python-programming/subnettree-output.txt"
+
+    # Read IP address file, match each address to longest prefix and print output
+    with open(filename, "a") as file:
+        for line in args.ip_address_file:
+            line = line.strip()
+            try:
+                print(line + "," + tree[line])
+                file.write(line + "," + tree[line])
+            except KeyError as e:
+                print("Skipped line '" + line + "'", file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
