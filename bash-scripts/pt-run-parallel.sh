73823446ddddf6f402c1a98da7941d2139527a2d
Erlend <erlend_hapnes@hotmail.com>
2022-03-18 19:04:12 +0100

    Fixed tracepath

--- bash-scripts/pt-run-parallel.sh
+++ bash-scripts/pt-run-parallel.sh
@@ -18,7 +18,7 @@ pt_test()
     local l_FILEPATH="/root/raw/"
     local l_FILENAME="$l_HOSTNAME-${l_SHORT}-${l_DATE}.txt"
     #echo $l_FILEPATH$l_FILENAME $l_HOSTNAME ${l_DESTINATION_PORT} ${l_HOST_IP} ${l_FLOW_LABEL} > "/root/test/$l_FILENAME"
-    tracepath -m $l_DESTINATION_ADDR > "/root/test/$l_FILENAME"
+    tracepath -m 8 $l_DESTINATION_ADDR > "/root/test/$l_FILENAME"
 }
 
 #pt_run()
