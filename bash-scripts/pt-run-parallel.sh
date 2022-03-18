0b8783bf3f6e42af81d85b25c1b628bf5bb8eae4
Erlend <erlend_hapnes@hotmail.com>
2022-03-18 19:02:53 +0100

    Update

--- bash-scripts/pt-run-parallel.sh
+++ bash-scripts/pt-run-parallel.sh
@@ -3,8 +3,8 @@
 LIST="/root/git/scripts/text-files/ipv6-address-list-pruned-2.txt"
 LIST_LENGTH=$(wc -l < $LIST) # get the number of lines in file
 #echo "$LIST_LENGTH"
-N=1
-let M=$N+9
+#N=1
+#let M=$N+9
 
 pt_test()
 {
@@ -21,20 +21,20 @@ pt_test()
     tracepath -m $l_DESTINATION_ADDR > "/root/test/$l_FILENAME"
 }
 
-pt_run()
-{
-    destination_address=$line
-    hash=$(echo -n ${destination_address} | md5sum | awk '{print $1}')
-    short="${hash:0:6}"
-    date=$(date '+%d-%H-%M-%S')
-    filepath="/root/raw/"
-    filename="$HOSTNAME-${short}-${date}.txt"
-    echo "Starting paris-traceroute"
-    sudo paris-traceroute -T -p ${destination_port} "${flow_label}" "${destination_address}" > $filepath$filename
-    echo "paris-traceroute finished. Output saved in $filename."
-    echo "Converting to JSON..."
-    python3 /root/git/scripts/python-scripts/text-to-json-2.py $filepath$filename $HOSTNAME ${destination_port} ${host_ip} ${flow_label}
-}
+#pt_run()
+#{
+    #destination_address=$line
+    #hash=$(echo -n ${destination_address} | md5sum | awk '{print $1}')
+    #short="${hash:0:6}"
+    #date=$(date '+%d-%H-%M-%S')
+    #filepath="/root/raw/"
+    #filename="$HOSTNAME-${short}-${date}.txt"
+    #echo "Starting paris-traceroute"
+    #sudo paris-traceroute -T -p ${destination_port} "${flow_label}" "${destination_address}" > $filepath$filename
+    #echo "paris-traceroute finished. Output saved in $filename."
+    #echo "Converting to JSON..."
+    #python3 /root/git/scripts/python-scripts/text-to-json-2.py $filepath$filename $HOSTNAME ${destination_port} ${host_ip} ${flow_label}
+#}
 
 # Explanation: 
 # In stage 2, the flow label will be constant (0).
@@ -53,14 +53,17 @@ FLOW_LABEL_MID_2=131071
 FLOW_LABEL_MAX=1048575
 
 #flow_labels=($FLOW_LABEL_MIN)
-flow_labels=($FLOW_LABEL_MID_2)
+FLOW_LABELS=($FLOW_LABEL_MID_2)
 #flow_labels=($FLOW_LABEL_MIN $FLOW_LABEL_MID_1 $FLOW_LABEL_MID_2 $FLOW_LABEL_MAX)
-destination_ports=($TRACEROUTE_DEFAULT_PORT $SSH_PORT $HTTP_PORT $HTTPS_PORT) # get destination tcp-port from input args
+DESTINATION_PORTS=($TRACEROUTE_DEFAULT_PORT $SSH_PORT) # get destination tcp-port from input args
+#DESTINATION_PORTS=($TRACEROUTE_DEFAULT_PORT $SSH_PORT $HTTP_PORT $HTTPS_PORT) # get destination tcp-port from input args
 # destination_port=$1 # get destination tcp-port from input args
 host_ip=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
 
 for DESTINATION_PORT in "${DESTINATION_PORTS[@]}"; do
     for FLOW_LABEL in "${FLOW_LABELS[@]}"; do
+        N=1
+        let M=$N+9
         while [ $N -lt $LIST_LENGTH ]; do
             readarray -t my_array < <(sed -n "${N},${M}p" $LIST)
                 for ADDRESS in ${my_array[@]}; do
