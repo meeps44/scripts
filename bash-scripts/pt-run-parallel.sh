e71d14255b063d8b6dccadb8aecc1b59c5a1d4df
Erlend <erlend_hapnes@hotmail.com>
2022-03-18 19:54:08 +0100

    Update

--- bash-scripts/pt-run-parallel.sh
+++ bash-scripts/pt-run-parallel.sh
@@ -18,7 +18,8 @@ pt_test()
     local l_FILENAME="$l_HOSTNAME-${l_SHORT}-${l_DATE}.txt"
     #echo $l_FILEPATH$l_FILENAME $l_HOSTNAME ${l_DESTINATION_PORT} ${l_HOST_IP} ${l_FLOW_LABEL} > "/root/test/$l_FILENAME"
     echo $l_DESTINATION_PORT $l_FLOW_LABEL $l_DESTINATION_ADDR
-    echo waffle $l_DESTINATION_PORT $l_FLOW_LABEL $l_DESTINATION_ADDR
+    echo waffle $l_DESTINATION_ADDR
+    echo tracepath -m 8 $l_DESTINATION_ADDR
     #tracepath -m 8 $l_DESTINATION_ADDR > "/root/test/$l_FILENAME"
 }
 
