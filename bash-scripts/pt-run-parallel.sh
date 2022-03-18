7737934567c521ed2eb93e7c9a17d41826d43001
Erlend <erlend_hapnes@hotmail.com>
2022-03-18 19:37:29 +0100

    Update

--- bash-scripts/pt-run-parallel.sh
+++ bash-scripts/pt-run-parallel.sh
@@ -8,7 +8,6 @@ LIST_LENGTH=$(wc -l < $LIST) # get the number of lines in file
 
 pt_test()
 {
-    echo "Hello from pt_test()"
     local l_DESTINATION_PORT=$1
     local l_FLOW_LABEL=$2
     local l_DESTINATION_ADDR=$3
@@ -16,13 +15,12 @@ pt_test()
     #echo "Executing tracepath. Dst addr: ${l_DESTINATION_ADDR}"
 
     local l_HASH=$(echo -n ${l_DESTINATION_ADDR} | md5sum | awk '{print $1}')
-    echo "Hash: $l_HASH"
     local l_SHORT="${l_HASH:0:6}"
     local l_DATE=$(date '+%d-%H-%M-%S')
     local l_FILEPATH="/root/raw/"
     local l_FILENAME="$l_HOSTNAME-${l_SHORT}-${l_DATE}.txt"
     #echo $l_FILEPATH$l_FILENAME $l_HOSTNAME ${l_DESTINATION_PORT} ${l_HOST_IP} ${l_FLOW_LABEL} > "/root/test/$l_FILENAME"
-    echo "Executing tracepath. Dst addr: $3"
+    echo Executing tracepath. Dst addr: $3
     #tracepath -m 8 $l_DESTINATION_ADDR > "/root/test/$l_FILENAME"
 }
 
