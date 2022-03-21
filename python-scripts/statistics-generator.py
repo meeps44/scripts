42751ee56535c49a7795287f27ef28f37ca4d30d
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-03-21 12:04:20 +0100

    Update

--- python-scripts/statistics-generator.py
+++ python-scripts/statistics-generator.py
@@ -19,36 +19,37 @@ args = parser.parse_args()
 elements = []
 ip_addresses = []
 iteration = 0
-number_of_paths = 0
-number_of_flow_labels = 0
+path_counter = 0 # number of unique paths found
+flow_label_counter = 0 # number of unique flow-labels found
 path_flow_set = {}
 
-tag = p.findall(args.file)
-first_tag = tag[0]
-print(f"Tag: {first_tag}")
+tags = p.findall(args.file)
+tag = tags[0] # we are assuming that the filename will not contain more than one tag, 
+# and that the directory will only contain certain kinds of logfiles specific to this project
+print(f"Tag: {tag}")
 print(f"Input Directory: {os.path.dirname(args.file)}")
 
 # open all files in a directory
 for filename in os.listdir(os.path.dirname(args.file)):
     # only open filenames containing the tag
-    if first_tag in filename:
-        print(f"Tag {first_tag} found in filename {filename}")
-        #with open(args.file, "r") as file:
-            ## returns JSON object as a dictionary
-            #data = json.load(file)
+    if tag in filename:
+        print(f"Tag {tag} found in filename {filename}")
+        with open(args.file, "r") as file:
+            # returns JSON object as a dictionary
+            data = json.load(file)
 
-            #source_ip = data['source']
-            #destination_ip = data['destination']
-            #tcp_port = data['outgoing_tcp_port']
-            #flow_label = data['flow_label']
-            ##returned_flow_label_1 = data1['hops']
+            source_ip = data['source']
+            destination_ip = data['destination']
+            tcp_port = data['outgoing_tcp_port']
+            flow_label = data['flow_label']
+            #returned_flow_label_1 = data1['hops']
 
-            #for key, value in data['hops'].items():
-                #elements.append(value)
-                #ip_addresses.append(data['hops'][key]['ipv6_address'])
+            for key, value in data['hops'].items():
+                elements.append(value)
+                ip_addresses.append(data['hops'][key]['ipv6_address'])
             
-            #print(elements)
-            #print(ip_addresses)
+            print(elements)
+            print(ip_addresses)
 
 #with open(args.file, "r") as file:
     ## returns JSON object as a dictionary
