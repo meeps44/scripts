c4b23e8c086d7536affd3038a821e278312897d9
Erlend <erlend_hapnes@hotmail.com>
2022-04-10 23:36:23 +0200

    Fixes

--- python-scripts/generate-hitlist-2.py
+++ python-scripts/generate-hitlist-2.py
@@ -13,18 +13,28 @@ except Exception as e:
     sys.exit(1)
 
 def get_asn(prefix, filename):
+    #for line in filename.readlines():
+        ##print("Entering for-loop")
+        ##print(f"{line=}")
+        #list = line.split()
+        #ip = list[0]
+        ##print(f"{ip=}")
+        #prefix_length = list[1]
+        ##print(f"{prefix_length=}")
+        #asn = list[2]
+        ##print(f"{asn=}")
+        #ip_with_prefix = ip + "/" + prefix_length
+
+        #if prefix == ip_with_prefix:
+            #return asn
+
     with open(filename, "r") as file:
         lines = file.readlines()
         for line in lines:
-            #print("Entering for-loop")
-            #print(f"{line=}")
             list = line.split()
             ip = list[0]
-            #print(f"{ip=}")
             prefix_length = list[1]
-            #print(f"{prefix_length=}")
             asn = list[2]
-            #print(f"{asn=}")
             ip_with_prefix = ip + "/" + prefix_length
 
             if prefix == ip_with_prefix:
@@ -48,7 +58,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--non-aliased-file", required=True, type=argparse.FileType('r'), help="File containing non-aliased prefixes")
     parser.add_argument("-i", "--ip-address-file", required=True, type=argparse.FileType('r'), help="File containing IP addresses to be matched against (non-)aliased prefixes")
-    parser.add_argument("-r", "--routeviews-file", required=True, type=argparse.FileType('r'), help="File containing full RouteViews data")
+    parser.add_argument("-r", "--routeviews-file", required=True, help="File containing full RouteViews data")
     args = parser.parse_args()
 
     # Store aliased and non-aliased prefixes in a single subnet tree
@@ -61,10 +71,7 @@ def main():
     for line in args.ip_address_file:
         line = line.strip()
         try:
-            print(f"{line=}")
-            print(f"{tree[line]=}")
-            print(f"Prefix length: {tree[line][-2:]}")
-            print(str(get_asn(tree[line], args.routeviews_file)))
+            print(f"{str(get_asn(tree[line], args.routeviews_file))} {tree[line][-2:]} {line} {tree[line]}")
             #print(line + ", " + tree[line] + ", " + str(get_asn(line, args.routeviews_file)))
         except KeyError as e:
             print("Skipped line '" + line + "'", file=sys.stderr)
