2ba160d1e894b5ab6e5ba8b54c224f5657dc5620
Erlend <erlend_hapnes@hotmail.com>
2022-04-11 20:23:03 +0200

    Update

--- bash-scripts/pt-run-parallel-2.sh
+++ bash-scripts/pt-run-parallel-2.sh
@@ -10,17 +10,18 @@ N_ITERATIONS = 1
 
 create_tarball()
 {
+    cd $HOME
     echo "Creating tarball..."
     local l_DATE=$(date '+%d-%m-%y-%H-%M-%S')
     local l_TAR_FILENAME="tar-$HOSTNAME-${l_DATE}.tar.gz"
     tar -czvf ${l_TAR_FILENAME} -C /root/logs/$HOSTNAME/ .
-    echo "Tarball saved to $(pwd)/$l_TAR_FILENAME. Cleaning up the /root/logs/$HOSTNAME/-directory..."
+    echo "Tarball saved to $HOME/$l_TAR_FILENAME. Cleaning up the /root/logs/$HOSTNAME/-directory..."
     rm /root/logs/$HOSTNAME/*
     echo "Transferring tarball to remote host"
     scp -i /root/.ssh/scp-key $HOME/$l_TAR_FILENAME 209.97.138.74:/root/archived-logs/$l_TAR_FILENAME
     if [ $? -eq 0 ];
     then
-        echo "Transfer completed successfully. Deleting tarball"
+        echo "Transfer completed successfully. Deleting tarball..."
         rm $HOME/$l_TAR_FILENAME 
         echo "Tarball deleted"
     else
