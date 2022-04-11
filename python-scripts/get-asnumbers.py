a89bf9340850ff3553bbcb179d406de20877407b
Erlend <erlend_hapnes@hotmail.com>
2022-04-11 23:27:48 +0200

    Update

--- python-scripts/get-asnumbers.py
+++ python-scripts/get-asnumbers.py
@@ -8,63 +8,23 @@ from __future__ import print_function
 import argparse
 import sys
 
-try:
-    import SubnetTree
-except Exception as e:
-    print(e, file=sys.stderr)
-    print("Use `pip install pysubnettree` to install the required module", file=sys.stderr)
-    sys.exit(1)
-
-
-def get_asns(filename):
+def get_asnumbers(filename):
+    as_numbers = {}
     for line in filename:
         list = line.split()
         ip = list[0]
         prefix_length = list[1]
         asn = list[2]
-        ip_with_prefix = ip + "/" + prefix_length
-
-        if prefix == ip_with_prefix:
-            return asn
-    return 0
-
-def read_non_aliased(tree, fh):
-    return fill_tree(tree, fh, ",0")
-
-def read_aliased(tree, fh):
-    return fill_tree(tree, fh, ",1")
-
-def fill_tree(tree, fh, suffix):
-    for line in fh:
-        line = line.strip()
-        try:
-            tree[line] = line + suffix
-        except ValueError as e:
-            print("Skipped line '" + line + "'", file=sys.stderr)
-    return tree
-
+        as_numbers.add(asn)
+    return as_numbers
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--aliased-file", required=True, type=argparse.FileType('r'), help="File containing aliased prefixes")
-    parser.add_argument("-n", "--non-aliased-file", required=True, type=argparse.FileType('r'), help="File containing non-aliased prefixes")
-    parser.add_argument("-i", "--ip-address-file", required=True, type=argparse.FileType('r'), help="File containing IP addresses to be matched against (non-)aliased prefixes")
+    parser.add_argument("-r", "--routeviews-file", required=True, type=argparse.FileType('r'), help="File containing RouteViews data")
+    parser.add_argument("-o", "--output-file", required=True, help="File we want to write the output to")
     args = parser.parse_args()
+    asns = get_asnumbers(args.routeviews_file)
 
-    # Store aliased and non-aliased prefixes in a single subnet tree
-    tree = SubnetTree.SubnetTree()
-
-    # Read aliased and non-aliased prefixes
-    tree = read_aliased(tree, args.aliased_file)
-    tree = read_non_aliased(tree, args.non_aliased_file)
-
-    # Read IP address file, match each address to longest prefix and print output
-    for line in args.ip_address_file:
-        line = line.strip()
-        try:
-            print(line + "," + tree[line])
-        except KeyError as e:
-            print("Skipped line '" + line + "'", file=sys.stderr)
 
 if __name__ == "__main__":
     main()
