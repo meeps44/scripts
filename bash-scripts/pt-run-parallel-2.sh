36bb2c7f3f0f4618abfbd3cfe6e2c44e98422444
Erlend <erlend_hapnes@hotmail.com>
2022-04-11 19:32:19 +0200

    Update to scp

--- bash-scripts/pt-run-parallel-2.sh
+++ bash-scripts/pt-run-parallel-2.sh
@@ -17,7 +17,7 @@ create_tarball()
     echo "Tarball saved to $(pwd)/$l_TAR_FILENAME. Cleaning up the /root/logs/$HOSTNAME/-directory..."
     rm /root/logs/$HOSTNAME/*
     echo "Transferring tarball to remote host"
-    scp -i <key> $HOME/$l_TAR_FILENAME {{remote_host}}:{{path/to/remote_file}}/root/archived-logs/$l_TAR_FILENAME
+    scp -i /root/.ssh/scp-key $HOME/$l_TAR_FILENAME 209.97.138.74:/root/archived-logs/$l_TAR_FILENAME
     if [ $? -eq 0 ];
     then
         echo "Transfer completed successfully. Deleting tarball"
