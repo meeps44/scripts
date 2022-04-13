2f608d85e645b91faacf68d592ba11451bc61f36
Erlend <erlend_hapnes@hotmail.com>
2022-04-14 01:33:29 +0200

    Update

--- bash-scripts/create-figures.sh
+++ bash-scripts/create-figures.sh
@@ -4,3 +4,9 @@
 
 # first find and unpack all zipped data
 tar -xf /root/archived-logs/tar-*
+
+# call a python-script to parse json-data
+python3 generate-statistics.py
+
+# call a python-script to generate CDF (done via matplotlib)
+python3 generate-CDF.py
