aaf8d3f5ac28f896187acc439300de4d49e2cbb1
Erlend <erlend_hapnes@hotmail.com>
2022-04-13 23:11:40 +0200

    Update

--- bash-scripts/pt-run-parallel-2.sh
+++ bash-scripts/pt-run-parallel-2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 HOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
-HITLIST="/root/git/scripts/text-files/ipv6-address-list-pruned-2.txt"
+HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
 HITLIST_LENGTH=$(wc -l < $HITLIST) # get the number of lines in file
 N_ITERATIONS = 1
 #echo "$HITLIST_LENGTH"
