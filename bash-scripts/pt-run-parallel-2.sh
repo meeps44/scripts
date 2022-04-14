7828347470bd49bc8a3d2c5b7865f0232dca9693
Erlend <erlend_hapnes@hotmail.com>
2022-04-14 15:54:02 +0200

    Updated hitlist

--- bash-scripts/pt-run-parallel-2.sh
+++ bash-scripts/pt-run-parallel-2.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 HOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
-HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
+#HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
+HITLIST="/root/git/scripts/text-files/hitlist.txt"
 HITLIST_LENGTH=$(wc -l < $HITLIST) # get the number of lines in file
 N_ITERATIONS=1
 TAR_DIR="/root/tarballs"
