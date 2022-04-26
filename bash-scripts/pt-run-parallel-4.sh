14f643c15e038e8f76b2971a79b9d023c89a0340
Erlend <erlend_hapnes@hotmail.com>
2022-04-26 21:29:51 +0200

    Update

--- bash-scripts/pt-run-parallel-4.sh
+++ bash-scripts/pt-run-parallel-4.sh
@@ -103,6 +103,7 @@ pt_run()
 
     echo "Starting paris-traceroute"
     sudo paris-traceroute --num-queries=1 -T -p ${l_DESTINATION_PORT} "${l_FLOW_LABEL}" "${l_DESTINATION_ADDR}" > $l_FILEPATH$l_FILENAME
+    #sudo paris-traceroute --first=2 --num-queries=1 -T -p ${l_DESTINATION_PORT} "${l_FLOW_LABEL}" "${l_DESTINATION_ADDR}" > $l_FILEPATH$l_FILENAME # skips the first router in the path
     echo "paris-traceroute finished. Output saved in $l_FILENAME."
     echo "Converting to JSON..."
     python3 /root/git/scripts/python-scripts/text-to-json-2.py $l_FILEPATH$l_FILENAME $HOSTNAME ${l_DESTINATION_PORT} ${HOST_IP} ${l_FLOW_LABEL}
