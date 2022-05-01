5d820bb1783794076c92b02a73f00be808513a7d
Erlend <erlend_hapnes@hotmail.com>
2022-05-01 23:22:06 +0200

    Update

--- python-scripts/measure-ipv6-responsiveness.py
+++ python-scripts/measure-ipv6-responsiveness.py
@@ -1,1 +1,20 @@
+import os
 # script that measurements the responsiveness of a list of IPv6-addresses
+
+def ping(hostnames, filename):
+    with open(filename, "w") as my_file:
+        for hostname in hostnames:
+            response = os.system("ping -c 1 " + hostname)
+            if response == 0:
+                print(f"{hostname} responded to ping")
+                my_file.write(hostname + "\n")
+            else:
+                print(f"{hostname} did not respond to ping")
+
+def main():
+    filename = "ip-responsiveness-test.txt"
+    hostnames = ["google.com"]
+    ping(hostnames, filename)
+
+if __name__ == "__main__":
+    main()
