00880402ac2d189dcdb8ba5b7facf09cefc16bd0
Erlend <erlend_hapnes@hotmail.com>
2022-05-04 15:23:30 +0200

    Updates to flow label definitions

--- bash-scripts/stage1.sh
+++ bash-scripts/stage1.sh
@@ -60,12 +60,9 @@ main()
 
 	# flow-label definitions
 	FLOW_LABEL_MIN=0
-	FLOW_LABEL_LOW_1=1
-	FLOW_LABEL_LOW_2=2
-	FLOW_LABEL_LOW_3=10
-	FLOW_LABEL_MID_1=277
-	FLOW_LABEL_MID_2=131071
-	FLOW_LABEL_HIGH_1=1048574
+	FLOW_LABEL_LOW=1
+	FLOW_LABEL_MID=256
+	FLOW_LABEL_HIGH=524288
 	FLOW_LABEL_MAX=1048575
 
 	# default values
@@ -91,7 +88,7 @@ main()
 	HITLIST_LENGTH=$(wc -l < $HITLIST) # get the number of lines in file
 	host_ip=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
 	FLOW_LABELS=($FLOW_LABEL_MIN $FLOW_LABEL_MIN $FLOW_LABEL_MIN $FLOW_LABEL_MIN)
-	DESTINATION_PORTS=($TRACEROUTE_DEFAULT_PORT) 
+	DESTINATION_PORTS=($HTTPS_PORT) 
 
 	# paris traceroute loop starts here
 	for i in $(seq 1 $N_ITERATIONS); do
