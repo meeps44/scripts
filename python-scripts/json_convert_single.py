52c9a9922b5430328e3ab14c48ffebc00b635ae5
Erlend <erlend_hapnes@hotmail.com>
2022-05-12 10:33:07 +0200

    Update

--- python-scripts/json_convert_single.py
+++ python-scripts/json_convert_single.py
@@ -24,7 +24,7 @@ def get_asn(tree, ip_address):
         #print(f"KeyError: {ip_address=} not found in subnettree", file=sys.stderr)
         return None
 
-def convert(tcp_port, source_ip, flow_label, data):
+def create_dict(directory, filename, tcp_port, source_ip, flow_label):
     # REGEX that matches IPv6-address. Credit: David M. Syzdek, https://gist.github.com/syzdek/6086792
     IPV4SEG  = r'(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])'
     IPV4ADDR = r'(?:(?:' + IPV4SEG + r'\.){3,3}' + IPV4SEG + r')'
@@ -50,54 +50,51 @@ def convert(tcp_port, source_ip, flow_label, data):
     reg = r"(((([0-9a-f]) ?){32})\n){6}"
     pattern = re.compile(reg, re.MULTILINE)
 
-    # file parsing starts here
-    match = re.search(pattern, data)
-
-    # create hop-list (list of ipv6_addresses in path)
-    hop_list = re.findall(IPV6ADDR, data) 
-    # remove duplicates
-    hop_list = list( dict.fromkeys(hop_list) )
-    # Remove first item in the list (the destination address) and add it as separate dictionary element
-    dest = hop_list.pop(0)
-
-    ## Initialize hop dictionary that only contains hops (used for generating path_id)
-    tmp_hop_dictionary = { index : {"ipv6_address" : address} for index, address in enumerate(hop_list, start=1)}
-
-    ## Create top-level dictionary
-    my_dict = {}
-    my_dict["outgoing_tcp_port"] = tcp_port
-    my_dict["flow_label"] = flow_label
-    my_dict["timestamp"] = str(datetime.datetime.now())
-    my_dict["source"] = source_ip
-    my_dict["source_asn"] = get_asn(tree, source_ip)
-    my_dict["destination"] = dest
-    my_dict["destination_asn"] = get_asn(tree, dest)
-    my_dict["path_id"] = hashlib.sha1(json.dumps(tmp_hop_dictionary, sort_keys=True).encode('utf-8')).hexdigest()
-
-    ## Initialize hop dictionary
-    hop_dictionary = { index : {"ipv6_address" : address, "asn" : "null", "returned_flow_label" : "null"} for index, address in enumerate(hop_list, start=1)}
-
-    # Find and append returned flow labels to the hop-dictionary
-    for item in re.finditer(pattern, data):
-        # print(item.group())
-        ip = (item.group()[24:72].replace(" ", "")).replace("\n", "") # use regex to find response-IP in txt file
-        ipv6_addr = ipaddress.ip_address(int(ip, 16))
-        fl = item.group()[151:158].replace(" ", "") # use regex to capture the returned flow-label contained in the ICMP payload. NB! will not work in the presence of IPv6 extension headers
-
-        for index, ip_address in enumerate(hop_list):
-            if (str(ip_address).replace(" ", "")).replace("\n", "") == (str(ipv6_addr).replace(" ", "")).replace("\n", ""):
-                hop_dictionary[index+1]["asn"] = get_asn(tree, ip_address)
-                hop_dictionary[index+1]["returned_flow_label"] = int(fl, 16)
-        
-    my_dict["hops"] = hop_dictionary
-    return my_dict
-
-def parse(directory, filename, tcp_port, source_ip, flow_label):
+    # File parsing starts here
     try:
         if (os.path.isfile(os.path.join(directory, filename))):
             with open(os.path.join(directory, filename), "r") as f:
-                file_data = f.read()
-                convert(tcp_port, source_ip, flow_label, file_data)
+                data = f.read()
+                match = re.search(pattern, data)
+
+                # Create hop-list (list of ipv6_addresses in path)
+                hop_list = re.findall(IPV6ADDR, data) 
+                # Remove duplicates
+                hop_list = list( dict.fromkeys(hop_list) )
+                # Remove first item in the list (the destination address) and add it as separate dictionary element
+                dest = hop_list.pop(0)
+
+                ## Initialize hop dictionary that only contains hops (used for generating path_id)
+                tmp_hop_dictionary = { index : {"ipv6_address" : address} for index, address in enumerate(hop_list, start=1)}
+
+                ## Create top-level dictionary
+                my_dict = {}
+                my_dict["outgoing_tcp_port"] = tcp_port
+                my_dict["flow_label"] = flow_label
+                my_dict["timestamp"] = str(datetime.datetime.now())
+                my_dict["source"] = source_ip
+                my_dict["source_asn"] = get_asn(tree, source_ip)
+                my_dict["destination"] = dest
+                my_dict["destination_asn"] = get_asn(tree, dest)
+                my_dict["path_id"] = hashlib.sha1(json.dumps(tmp_hop_dictionary, sort_keys=True).encode('utf-8')).hexdigest()
+
+                ## Initialize hop dictionary
+                hop_dictionary = { index : {"ipv6_address" : address, "asn" : "null", "returned_flow_label" : "null"} for index, address in enumerate(hop_list, start=1)}
+
+                # Find and append returned flow labels to the hop-dictionary
+                for item in re.finditer(pattern, data):
+                    # print(item.group())
+                    ip = (item.group()[24:72].replace(" ", "")).replace("\n", "") # use regex to find response-IP in txt file
+                    ipv6_addr = ipaddress.ip_address(int(ip, 16))
+                    fl = item.group()[151:158].replace(" ", "") # use regex to capture the returned flow-label contained in the ICMP payload. NB! will not work in the presence of IPv6 extension headers
+
+                    for index, ip_address in enumerate(hop_list):
+                        if (str(ip_address).replace(" ", "")).replace("\n", "") == (str(ipv6_addr).replace(" ", "")).replace("\n", ""):
+                            hop_dictionary[index+1]["asn"] = get_asn(tree, ip_address)
+                            hop_dictionary[index+1]["returned_flow_label"] = int(fl, 16)
+                    
+                my_dict["hops"] = hop_dictionary
+
     except FileNotFoundError:
         print("Error: No such file or directory")
         exit(1)
@@ -105,6 +102,8 @@ def parse(directory, filename, tcp_port, source_ip, flow_label):
         print("Error: Not a directory")
         print("Please use the --file option to compare single files. Use the -h argument for more info.")
         exit(1)
+
+    return my_dict
 
 def create_filename(hostname):
     now = datetime.datetime.now()
@@ -130,7 +129,7 @@ def main():
 
     hostname = str(socket.gethostname())
 
-    json_data = parse(args.directory, args.file, args.tcp_port, args.source_ip, args.flow_label)
+    json_data = create_dict(args.directory, args.file, args.tcp_port, args.source_ip, args.flow_label)
     my_filename = create_filename(hostname)
     fwrite(json_data, my_filename)
 
