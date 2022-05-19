7a41db88ac7196c7b84d8bccf11b941e38448ee8
Erlend <erlend_hapnes@hotmail.com>
2022-05-19 17:58:19 +0200

    Updated print statements

--- python-scripts/identify-paths-dev-2.py
+++ python-scripts/identify-paths-dev-2.py
@@ -161,11 +161,18 @@ def main():
             for key in test_dict:
                 if len(test_dict[key]) == 1:
                     #print(f"Number of paths from {source=} to destination {key} with {flow_label=}: {len(test_dict[key])}")
-                    print(f"Source {source} destination {key} flow_label {flow_label} Number_of_unique_paths_to_destination {len(get_unique(test_dict[key]))} Hop_number_where_paths_diverged 0")
-                    print(f"List of ASes traversed: {asn_list}")
-                    print(f"List of unique ASes traversed: {unique_asn_list}")
-                    print(f"Number of ASes traversed: {len(asn_list)}")
-                    print(f"Number of unique ASes traversed: {len(unique_asn_list)}")
+                    print(f"Legend: \
+                        Source \
+                        destination \
+                        flow_label \
+                        Number_of_unique_paths_to_destination \
+                        Hop_number_where_paths_diverged \
+                        List_of_ASes_traversed \
+                        Number_of_ASes_traversed \
+                        Number of unique ASes traversed\
+                        ")
+                    print(f"{source} {key} {flow_label} {len(get_unique(test_dict[key]))} 0 {asn_list} {unique_asn_list} {len(asn_list)} {len(unique_asn_list)}")
+                    #print(f"Source {source} destination {key} flow_label {flow_label} Number_of_unique_paths_to_destination {len(get_unique(test_dict[key]))} Hop_number_where_paths_diverged 0")
                 if len(test_dict[key]) > 1:
                     divergence_dictionary[key] = []
 
@@ -193,23 +200,36 @@ def main():
                                 hop_list_of_lists.append(new_list)
                 divergence_dictionary[key] = path_id_list
                 if len(divergence_dictionary[key]) == 1:
-                    print(f"Source {source} destination {key} flow_label {flow_label} Number_of_unique_paths_to_destination {len(get_unique(divergence_dictionary[key]))} Hop_number_where_paths_diverged 0")
-                    print(f"List of ASes traversed: {asn_list}")
-                    print(f"List of unique ASes traversed: {unique_asn_list}")
-                    print(f"Number of ASes traversed: {len(asn_list)}")
-                    print(f"Number of unique ASes traversed: {len(unique_asn_list)}")
+                    #print(f"Source {source} destination {key} flow_label {flow_label} Number_of_unique_paths_to_destination {len(get_unique(divergence_dictionary[key]))} Hop_number_where_paths_diverged 0")
+                    print(f"Legend: \
+                        Source \
+                        destination \
+                        flow_label \
+                        Number_of_unique_paths_to_destination \
+                        Hop_number_where_paths_diverged \
+                        List_of_ASes_traversed \
+                        Number_of_ASes_traversed \
+                        Number of unique ASes traversed\
+                        ")
+                    print(f"{source} {key} {flow_label} {len(get_unique(test_dict[key]))} 0 {asn_list} {unique_asn_list} {len(asn_list)} {len(unique_asn_list)}")
                 if len(divergence_dictionary[key]) > 1:
                     tmp = compare_list_of_lists(hop_list_of_lists)
                     div_list = []
                     for item in tmp:
                         div_list.append(item+1) # to correct mismatch between hop number and list index, we increment by 1
                     if div_list:
-                        print(f"Source {source} destination {key} flow_label {flow_label} Number_of_unique_paths_to_destination {len(get_unique(divergence_dictionary[key]))} Hop_number_where_paths_diverged {div_list}")
-                        print(f"List of ASes traversed: {asn_list}")
-                        print(f"List of unique ASes traversed: {unique_asn_list}")
-                        print(f"Number of ASes traversed: {len(asn_list)}")
-                        print(f"Number of unique ASes traversed: {len(unique_asn_list)}")
-                        #print(f"List of hop numbers where the paths diverged: {div_list}")
+                        #print(f"Source {source} destination {key} flow_label {flow_label} Number_of_unique_paths_to_destination {len(get_unique(divergence_dictionary[key]))} Hop_number_where_paths_diverged {div_list}")
+                        print(f"Legend: \
+                            Source \
+                            destination \
+                            flow_label \
+                            Number_of_unique_paths_to_destination \
+                            Hop_number_where_paths_diverged \
+                            List_of_ASes_traversed \
+                            Number_of_ASes_traversed \
+                            Number of unique ASes traversed\
+                            ")
+                        print(f"{source} {key} {flow_label} {len(get_unique(test_dict[key]))} {div_list} {asn_list} {unique_asn_list} {len(asn_list)} {len(unique_asn_list)}")
 
 if __name__ == "__main__":
     main()
