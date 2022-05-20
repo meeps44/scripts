5d6d1ef6fc649045d2924f20e944b4a8fa77e122
Erlend <erlend_hapnes@hotmail.com>
2022-05-20 13:16:51 +0200

    Update

--- python-scripts/generate-cdf.py
+++ python-scripts/generate-cdf.py
@@ -6,12 +6,15 @@ import pandas as pd
 filepath = "C:\\Users\\Erlend\\Documents\\Programming\\Python programming\\data.txt"
 with open(filepath, "r") as file:
     data = file.readlines()
+    data = list(map(int, data)) # Convert input data from str to int
     N = len(data)
-    arr = np.array(data)
 
-X2 = np.sort(data)
-F2 = np.array(range(N))/float(N)
+X = np.sort(data)
+Y = np.array(range(N))/float(N)
 
-plt.plot(X2, F2, label="CDF")
+plt.plot(X, Y, label="CDF")
+plt.xticks(np.arange(min(data), max(data)+1, 1.0))
+plt.xlabel("Hop number")
+plt.ylabel("Probability that the amount of hop numbers is less than H")
 plt.legend()
 plt.show()
