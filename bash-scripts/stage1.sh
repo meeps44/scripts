5d7bdb99c35975f7926c5eeb3cb8b354a552af0f
Erlend <erlend_hapnes@hotmail.com>
2022-05-31 22:33:59 +0200

    Updates

--- bash-scripts/stage1.sh
+++ bash-scripts/stage1.sh
@@ -78,6 +78,7 @@ main()
         $FLOW_LABEL_HIGH 
         )
 	DESTINATION_PORTS=($HTTPS_PORT) 
+	#FLOW_LABELS=($FLOW_LABEL_MIN $FLOW_LABEL_MIN $FLOW_LABEL_MIN $FLOW_LABEL_MIN)
 	HITLIST="/root/git/scripts/text-files/responsive-alexatop500-addresses-3105.txt"
 	
 
@@ -97,8 +98,6 @@ main()
 	TAR_DIR="/root/tarballs"
 	HITLIST_LENGTH=$(wc -l < $HITLIST) # get the number of lines in file
 	host_ip=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
-	FLOW_LABELS=($FLOW_LABEL_MIN $FLOW_LABEL_MIN $FLOW_LABEL_MIN $FLOW_LABEL_MIN)
-	DESTINATION_PORTS=($HTTPS_PORT) 
 
 	# paris traceroute loop starts here
 	for i in $(seq 1 $N_ITERATIONS); do
