2d8b0ed9011c543109516dc2b9a3806f637368d9
Erlend <erlend_hapnes@hotmail.com>
2022-09-13 20:10:13 +0200

    Updated N iterations

--- september/pt-run-parallel.sh
+++ september/pt-run-parallel.sh
@@ -96,10 +96,10 @@ fi
 
 if [ "$FULL_HITLIST" = true ]; then
     # Short hitlist (20 lines)
-    HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
+    #HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
 
     # Long hitlist (15757 lines)
-    #HITLIST="/root/git/scripts/text-files/hitlist.txt"
+    HITLIST="/root/git/scripts/text-files/hitlist.txt"
 else
     # Short hitlist (Alexa top 500)
     HITLIST="/root/git/scripts/text-files/ipv6-address-list-alexa-top500-pruned.txt"
@@ -108,7 +108,7 @@ fi
 # Other definitions
 LOCALHOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
 TAR_DIR="/root/tarballs"
-N_ITERATIONS=1 # The number of iterations that you wish to run the experiment.
+N_ITERATIONS=4 # The number of iterations that you wish to run the experiment.
 HITLIST_LENGTH=$(wc -l <$HITLIST)
 DATE=$(date '+%Y-%m-%dT%H_%M_%SZ')
 CSV_FILEPATH="/root/csv/"
