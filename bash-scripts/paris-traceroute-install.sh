34e0928923c27994b0f7de7aaa2d9f5878278804
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-09-17 13:20:51 +0200

    Updates to install script

--- bash-scripts/paris-traceroute-install.sh
+++ bash-scripts/paris-traceroute-install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir ~/raw
-mkdir -p ~/logs/$HOSTNAME
-mkdir ~/git
-cd ~/git
-git clone https://github.com/meeps44/libparistraceroute.git
-cd ~/git/libparistraceroute
+mkdir /root/csv /root/csv-storage /root/git /root/raw /root/logs
+sudo apt -y install libssl-dev
+git -C /root/git clone https://github.com/meeps44/libparistraceroute.git
+cd /root/git/libparistraceroute
 mkdir m4
 ./autogen.sh
 ./configure
@@ -14,5 +12,5 @@ make
 make install
 export PATH=$PATH:~/git/libparistraceroute/paris-traceroute
 ldconfig
-apt -y install python3-pip
-python3 -m pip install pysubnettree
+#apt -y install python3-pip
+#python3 -m pip install pysubnettree
