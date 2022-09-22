fb4aaf49a90735d117ca34a6da67e7441e1611fa
Erlend <erlend_hapnes@hotmail.com>
2022-09-22 20:38:19 +0200

    small update

--- september/download-archived-logs.sh
+++ september/download-archived-logs.sh
@@ -2,7 +2,7 @@
 # Downloads archived logs from salt master server
 
 LOCAL_DIR="$HOME/csv-storage/"
-REMOTE_DIR="/root/csv-storage/"
+REMOTE_DIR="/root/csv-storage/*"
 REMOTE_HOST="209.97.138.74"
 
 scp -i "/home/erlend/.ssh/scp-key" -r root@$REMOTE_HOST:$REMOTE_DIR $LOCAL_DIR
