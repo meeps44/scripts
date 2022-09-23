d8ccef3f736f935e5ad49f72794e34224cd0fc4a
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-09-23 09:09:41 +0200

    Minor fix

--- september/download-archived-logs.sh
+++ september/download-archived-logs.sh
@@ -5,5 +5,5 @@ LOCAL_DIR="$HOME/csv-storage/"
 REMOTE_DIR="/root/csv-storage/*"
 REMOTE_HOST="209.97.138.74"
 
-scp -i "/home/erlend/.ssh/scp-key" -r root@$REMOTE_HOST:$REMOTE_DIR $LOCAL_DIR
+scp -i "$HOME/.ssh/scp-key" -r root@$REMOTE_HOST:$REMOTE_DIR $LOCAL_DIR
 echo "Files saved to $LOCAL_DIR."
