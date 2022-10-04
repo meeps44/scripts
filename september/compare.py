f2297e8ea1124f9442d8cf12dea4a9defce40ee9
Erlend <erlend_hapnes@hotmail.com>
2022-10-04 21:06:04 +0200

    added compare.py

---
+++ september/compare.py
@@ -0,0 +1,10 @@
+import dataclasses
+
+# Each path object is a list of tuples consisting of the hop number and
+# the hop address.
+# We are assuming that the paths all have the same
+# (source, destination, flow label, destination port number) 4-tuple.
+
+
+def compare_paths(path1, path2, path3, path4):
+    pass
