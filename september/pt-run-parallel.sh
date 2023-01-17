691222a69407210ab17777c57305e284d03fdb08
Erlend <erlend_hapnes@hotmail.com>
2023-01-17 20:03:27 +0100

    Added send_db()

--- september/pt-run-parallel.sh
+++ september/pt-run-parallel.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-# Exit when any command fails
-#set -e
-## Keep track of the last executed command
-#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-## Echo an error message before exiting
-#trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# Test definitions:
+TEST=true
+LOCALHOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
+TAR_DIR="/root/tarballs"
+N_ITERATIONS=4 # The number of iterations that you wish to run the experiment.
+N_PARALLEL=10  # Number of parallel Paris traceroute instances.
+HITLIST_LENGTH=$(wc -l <$HITLIST)
+DATE=$(date '+%Y-%m-%dT%H_%M_%SZ')
+DB_FILEPATH="/root/db/"
+DB_FILENAME="db-$HOSTNAME-$DATE.db"
+TAR_FILENAME="tar-$HOSTNAME-$DATE.tar.gz"
+#START_TIME=$(date '+%s')
+#HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
 
 # Port definitions:
 TRACEROUTE_DEFAULT_PORT=33434
@@ -25,54 +33,18 @@ FLOW_LABEL_3=983040 #(decimal), F0000 hex
 #1111 11111111 11111111:
 FLOW_LABEL_4=1048575 #(decimal), FFFFF hex
 
-# Flow label and port values
+# Flow label and port value definitions:
 FLOW_LABELS=($FLOW_LABEL_0 $FLOW_LABEL_1 $FLOW_LABEL_2 $FLOW_LABEL_3 $FLOW_LABEL_4)
 DESTINATION_PORTS=($HTTPS_PORT)
-HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
-
-# Use large or small hitlist
-FULL_HITLIST=false
-
-# Experiment stages
-# Stage 1
-# The goal of stage 1 is to figure out if the flow-label is maintained across all hops to a destination
-# Stage 2
-# In stage 2, the flow label will be constant (0).
-# The goal is to figure out if we can get a change in path by changing the port number
-# Stage 3
-# The goal of this step is to delve into the cases from stage 2
-# We want to know if we get the same path if we use a different port-number. Mix of well-known ports
-STAGE2=false
-
-if [ "$STAGE2" = true ]; then
-    # Stage 2
-    # In stage 2, the flow label will be constant (0).
-    # The goal is to figure out if we can get a change in path by changing the port number
-    FLOW_LABELS=($FLOW_LABEL_0)
-    DESTINATION_PORTS=($TRACEROUTE_DEFAULT_PORT $SSH_PORT $HTTP_PORT $HTTPS_PORT $DNS_PORT)
-fi
-
-if [ "$FULL_HITLIST" = true ]; then
+if [ "$TEST" = true ]; then
     # Short hitlist (20 lines)
-    #HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
-
+    HITLIST="/root/git/scripts/text-files/short_hitlist.txt"
+    # Short hitlist (112 lines)
+    # HITLIST="/root/git/scripts/text-files/ipv6-address-list-alexa-top500-pruned.txt"
+else
     # Long hitlist (15757 lines)
     HITLIST="/root/git/scripts/text-files/hitlist.txt"
-else
-    # Short hitlist (Alexa top 500)
-    HITLIST="/root/git/scripts/text-files/ipv6-address-list-alexa-top500-pruned.txt"
 fi
-
-# Other definitions:
-LOCALHOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
-TAR_DIR="/root/tarballs"
-N_ITERATIONS=4 # The number of iterations that you wish to run the experiment.
-HITLIST_LENGTH=$(wc -l <$HITLIST)
-DATE=$(date '+%Y-%m-%dT%H_%M_%SZ')
-CSV_FILEPATH="/root/csv/"
-DB_FILEPATH="/root/db/"
-DB_FILENAME="db-$HOSTNAME-$DATE.db"
-TAR_FILENAME="tar-$HOSTNAME-$DATE.tar.gz"
 
 create_tarball() {
     cd $TAR_DIR
@@ -86,10 +58,23 @@ create_tarball() {
         rm $TAR_DIR/$TAR_FILENAME
         echo "Tarball deleted."
         echo "Cleaning up raw data..."
-        #find /root/csv/ -maxdepth 1 -name "*.csv" -print0 | xargs -0 rm
         find /root/db/ -maxdepth 1 -name "*.db" -print0 | xargs -0 rm
     else
         echo "Transfer to remote host failed."
+    fi
+}
+
+send_db() {
+    cd $TAR_DIR
+    echo "Compressing DB and creating tarball..."
+    tar -czvf $TAR_FILENAME -C /root/db/$DB_FILEPATH$DB_FILENAME .
+    echo "Tarball saved to $TAR_DIR/$TAR_FILENAME."
+    echo "Transferring tarball to remote host..."
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /root/.ssh/scp-key $TAR_DIR/$TAR_FILENAME 209.97.138.74:/root/db-storage/$TAR_FILENAME
+    if [ $? -eq 0 ]; then
+        echo "Tarball transfer to remote host completed successfully."
+    else
+        echo "Tarball transfer to remote host failed."
     fi
 }
 
@@ -97,34 +82,37 @@ pt_run() {
     local l_DESTINATION_ADDR="$1"
     local l_DESTINATION_PORT="$2"
     local l_FLOW_LABEL="$3"
-    local START_TIME=$(date '+%s')
 
-    echo "Starting paris-traceroute."
+    # Start Paris traceroute #
     sudo /root/git/libparistraceroute/paris-traceroute/paris-traceroute --num-queries=1 -T -p $l_DESTINATION_PORT $START_TIME $LOCALHOST_IP $DB_FILEPATH$DB_FILENAME $l_FLOW_LABEL $l_DESTINATION_ADDR >/dev/null
-    echo "Paris-traceroute finished. Output saved to $DB_FILEPATH$DB_FILENAME."
+    echo "Paris traceroute finished. Output saved to $DB_FILEPATH$DB_FILENAME."
 }
 
-test -f /root/time.txt || touch /root/time.txt
-echo "Start time: $(date)" >>/root/time.txt
-
-for DESTINATION_PORT in "${DESTINATION_PORTS[@]}"; do
-    N=1
-    M=10
-    while [ $N -lt $HITLIST_LENGTH ]; do
-        readarray -t my_array < <(sed -n "${N},${M}p" $HITLIST)
-        for FLOW_LABEL in "${FLOW_LABELS[@]}"; do
-            for i in $(seq 1 $N_ITERATIONS); do
-                for ADDRESS in ${my_array[@]}; do
-                    pt_run "$ADDRESS" "$DESTINATION_PORT" "$FLOW_LABEL" &
+main() {
+    for DESTINATION_PORT in "${DESTINATION_PORTS[@]}"; do
+        N=1
+        M=$N_PARALLEL
+        while [ $N -lt $HITLIST_LENGTH ]; do
+            START_TIME=$(date '+%s')
+            readarray -t my_array < <(sed -n "${N},${M}p" $HITLIST)
+            for FLOW_LABEL in "${FLOW_LABELS[@]}"; do
+                for i in $(seq 1 $N_ITERATIONS); do
+                    for ADDRESS in ${my_array[@]}; do
+                        pt_run "$ADDRESS" "$DESTINATION_PORT" "$FLOW_LABEL" &
+                    done
+                    wait
                 done
-                wait
             done
+            let N=$N+$N_PARALLEL
+            let M=$M+$N_PARALLEL
         done
-        let N=$N+10
-        let M=$M+10
     done
-done
-wait
-echo "End time: $(date)" >>/root/time.txt
-# create_tarball
+    wait
+    echo "End time: $(date)" >>/root/time.log
+}
+
+test -f /root/time.log || touch /root/time.log
+echo "Start time: $(date)" >>/root/time.log
+main
+send_db
 echo "All done!"
