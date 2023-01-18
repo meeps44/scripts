877f6f8852b21469138612a0a2021fcfd25ba68a
Erlend <erlend_hapnes@hotmail.com>
2023-01-18 22:04:58 +0100

    Bumped N_ITERATIONS to 2, N_PARALLEL to 8

--- september/pt-run-parallel.sh
+++ september/pt-run-parallel.sh
@@ -4,8 +4,8 @@
 TEST=true
 LOCALHOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
 TAR_DIR="/root/tarballs"
-N_ITERATIONS=1 # The number of iterations that you wish to run the full experiment.
-N_PARALLEL=5   # Number of parallel Paris traceroute instances.
+N_ITERATIONS=2 # The number of iterations that you wish to run the full experiment.
+N_PARALLEL=8   # Number of parallel Paris traceroute instances.
 DATE=$(date '+%Y-%m-%dT%H_%M_%SZ')
 DB_FILEPATH="/root/db/"
 DB_FILENAME="db-$HOSTNAME-$DATE.db"
@@ -63,7 +63,7 @@ create_tarball() {
 
 transfer_db() {
     cd $TAR_DIR
-    echo "Compressing DB and creating tarball..."
+    echo "Creating tarball..."
     tar -czvf $TAR_FILENAME -C /root/db/$DB_FILEPATH$DB_FILENAME .
     echo "Tarball saved to $TAR_DIR/$TAR_FILENAME."
     echo "Transferring tarball to remote host..."
@@ -111,5 +111,5 @@ test -f /root/time.log || touch /root/time.log
 echo "Start time: $(date)" >>/root/time.log
 main
 echo "End time: $(date)" >>/root/time.log
-#transfer_db
+transfer_db
 echo "All done!"
