594faa662dce41fc6a1aac34f7e07dda28b61b6d
Erlend <erlend_hapnes@hotmail.com>
2023-01-19 23:57:20 +0100

    Set N_ITERATIONS to 1 and set TEST to false

--- september/pt-run-parallel.sh
+++ september/pt-run-parallel.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Test definitions:
-TEST=true
+TEST=false
 LOCALHOST_IP=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
 TAR_DIR="/root/tarballs/"
-N_ITERATIONS=2 # The number of iterations that you wish to run the full experiment.
+N_ITERATIONS=1 # The number of iterations that you wish to run the full experiment.
 N_PARALLEL=8   # Number of parallel Paris traceroute instances.
 DATE=$(date '+%Y-%m-%dT%H_%M_%SZ')
 DB_FILEPATH="/root/db/"
