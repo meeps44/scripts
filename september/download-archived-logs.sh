fa85265de2f7218b66dbd27b943ace6ce86e2724
Erlend <erlend_hapnes@hotmail.com>
2023-01-19 23:06:34 +0100

    Various fixes

--- september/download-archived-logs.sh
+++ september/download-archived-logs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Downloads archived logs from salt master server
 
-LOCAL_DIR="$HOME/csv-storage/"
-REMOTE_DIR="/root/csv-storage/*"
+LOCAL_DIR="$HOME/db-storage/"
+REMOTE_DIR="/root/db-storage/*"
 REMOTE_HOST="209.97.138.74"
 
 scp -i "$HOME/.ssh/scp-key" -r root@$REMOTE_HOST:$REMOTE_DIR $LOCAL_DIR
