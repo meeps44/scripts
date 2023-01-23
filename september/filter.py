c60ebfc8d8d47efae03c20269ae25692e02335db
Erlend <erlend_hapnes@hotmail.com>
2023-01-23 21:52:10 +0100

    Small update

--- september/filter.py
+++ september/filter.py
@@ -1,3 +1,5 @@
+import glob
+#import os
 from dataclasses import dataclass
 from sqlite3 import connect
 import pandas as pd
@@ -139,15 +141,17 @@ def sqlite_exec(conn, query) -> pd.DataFrame:
 def main():
     stats = TracerouteStatistics()
     source_flow_labels = [0, 255, 1048575]
-    filepath = "/home/erlhap/test/python/paris-traceroute-filter/data/"
-    filename = "db-ubuntu-ams3-0-2023-01-19T22_32_20Z.db"
-    conn = sqlite_init(filepath + filename)
-    unique_start_times = get_unique_start_times(conn)
-    for time in unique_start_times:
-        for fl in source_flow_labels:
-            df = sqlite_exec(
-                conn, f"SELECT PATH_HASH FROM TRACEROUTE_DATA WHERE START_TIME={time} AND SOURCE_FLOW_LABEL={fl}")
-    print(df)
+    # Get all databases in directory.
+    ls: list = glob.glob(
+        "/home/erlhap/test/python/paris-traceroute-filter/data/*.db")
+    for db_file in ls:
+        conn = sqlite_init(db_file)
+        unique_start_times = get_unique_start_times(conn)
+        for time in unique_start_times:
+            for fl in source_flow_labels:
+                df = sqlite_exec(
+                    conn, f"SELECT PATH_HASH FROM TRACEROUTE_DATA WHERE START_TIME={time} AND SOURCE_FLOW_LABEL={fl}")
+        print(df)
 
 
 if __name__ == "__main__":
