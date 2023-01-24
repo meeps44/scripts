e69d0e4c54952320ff7348b02a6f691820cc1ee8
Erlend Hapnes <erlend_hapnes@hotmail.com>
2023-01-24 13:59:20 +0100

    Update distributions.py

--- stats-app/statistics/distributions.py
+++ stats-app/statistics/distributions.py
@@ -1,3 +1,4 @@
+from filter import sqlite_init, sqlite_exec
 from dataclasses import dataclass
 from sqlite3 import connect
 import pandas as pd
@@ -10,10 +11,39 @@ class TracerouteStatistics:
     num_loops: int = 0
     num_fl_changes: int = 0
 
-def number_of_equal_paths(flowlabel: int):
+def compare_path_hash(df: pd.DataFrame, flowlabel: int, start_time: int):
+    """
+    Compare the path hash of all paths in the dataset with the same START_TIME and SOURCE_FLOW_LABEL.
+    Return the number of instances where the path stayed the same.
+    """
+    indices = list()
+    src_fl: str = df['SOURCE_FLOW_LABEL']
+    for row_idx in df.index:
+        hrfl: str = df['HOP_RETURNED_FLOW_LABELS'][row_idx]
+        flow_labels: list = hrfl.split(" ")
+        for val in flow_labels:
+            if src_fl != val:
+                indices.append(row_idx)
+                break
+    return indices
+
+def get_number_of_instances_where_path_stayed_consistent(df: pd.DataFrame, flowlabel: int):
+    match flowlabel:
+        case 0:
+            pass
+        case 255:
+            pass
+        case 65280:
+            pass
+        case 983040:
+            pass
+        case 1048575:
+            pass
+
+def get_number_of_equal_paths(df: pd.DataFrame, flowlabel: int):
     pass
 
-def hop_where_path_diverged(flowlabel: int):
+def get_hop_where_path_diverged(df: pd.DataFrame, flowlabel : int):
     pass
 
 def main():
