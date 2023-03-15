8f2f3b919e66f2552b02396c92589861d730f1b8
Erlend <erlend_hapnes@hotmail.com>
2023-03-16 00:20:49 +0100

    Added get_cycle_indices to test

--- stats/tests/test_count_cycles.py
+++ stats/tests/test_count_cycles.py
@@ -117,17 +117,23 @@ def count_cycles(df: pd.DataFrame) -> int:
         for idx, column in enumerate(hop_ip_addresses):
             i = idx
             # Incrementing by one to avoid comparing against the next IP-address (which would make it a loop)
-            i += 1
+            i += 2
             while i < length:
                 if column == hop_ip_addresses[i]:
+                    # print("Cycle detected")
+                    # print(f"{row[0]=}")
+                    # print(f"{idx=}\n{i=}")
+                    # print(f"{column=}\n{hop_ip_addresses[i]=}")
+                    # print(f"{idx=}\n{i=}")
+                    # print(f"{hop_ip_addresses=}")
                     cycle_count += 1
+                    # time.sleep(1000)
                     break
                 i += 1
-            time.sleep(1000)
     return cycle_count
 
 
-def get_cycle_indices(df: pd.DataFrame) -> list:
+def get_cycle_rows(df: pd.DataFrame) -> list:
     cycle_indices = list()
     for row in df.itertuples():
         break_out_flag = False
@@ -138,14 +144,15 @@ def get_cycle_indices(df: pd.DataFrame) -> list:
                 break
             i = idx
             # Incrementing by one to avoid comparing against the next IP-address (which would make it a loop)
-            i += 1
+            i += 2
             while i < length:
                 if column == hop_ip_addresses[i]:
-                    cycle_indices.append(idx)
+                    cycle_indices.append(row[0])
                     break_out_flag = True
                     break
                 i += 1
     return cycle_indices
+
 
     # print("Counting cycles")
     # num_cycles = count_cycles(data)
@@ -155,9 +162,12 @@ def get_cycle_indices(df: pd.DataFrame) -> list:
     # print(f"Cycle indices:\n{cycle_indices}")
 test_data = ('fd00:0:1::266', '2604:a880:ffff:4:1::46', '2604:a880:ffff:4:1::34', '2604:a880:ffff:4:1::46', '2a01:3e0:30::35', '2a01:3e0:ff40:200::21', '2001:5a0:2000:500::22', '2001:5a0:2000:500::52', '2001:5a0:a00::32', '2001:470:0:387::2', '2001:470:0:387::2', '2001:5a0:40:300::65', '2001:1278:1::3ba', '2001:550:0:1000::9a36:3d5',
              '2001:550:0:1000::9a36:5076', '2001:550:0:1000::9a36:29ca', '2001:550:2:85::39:2', '2001:550:0:1000::9a36:29ca', '2001:1260:100:15::1e', '2001:1260:100:15::31', '2604:d600:ff00:5a::25', '2806:20d:50ff:75::2', '2001:1218:2000:4f0::1', '2001:1218:3000:2f0::a6', '2001:1218:420c::a', '2001:1218:1000:f0::9')
-cycle_count = test_count_cycles(test_data)
+# cycle_count = test_count_cycles(test_data)
+# print(f"cycle count: {cycle_count}")
+cycle_count = count_cycles(data)
 print(f"cycle count: {cycle_count}")
-# count_cycles(data)
+cycle_indices = get_cycle_rows(data)
+print(f"cycle indices: {cycle_indices}")
 
 
 def f():
